44618fc03f94cc2910bf82169a11a5088f66ad1f
Ryan Mukherjee <mukhery@gmail.com>
2024-12-24 11:09:08 -0500

    run pip install in kornia-py

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ cargo-bundle-licenses \
   --format yaml \
   --output ${SRC_DIR}/THIRDPARTY_LICENSES.yaml
 
+cd kornia-py
 # Run the maturin build via pip which works for direct and
 # cross-compiled builds.
 $PYTHON -m pip install . -vv
